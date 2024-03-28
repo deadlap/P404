706bd1e1405c70e086f5fda56755155d3478d398
Akaidus <nikolajjensen6854@gmail.com>
2024-03-28 10:46:26 +0100

    Update ActivateBook.cs

--- Assets/Scripts/Book/ActivateBook.cs
+++ Assets/Scripts/Book/ActivateBook.cs
@@ -4,9 +4,13 @@ public class ActivateBook : MonoBehaviour
 {
     GameObject nonDominantHand;
     [SerializeField] bool toggleFollow;
+    Vector3 initialPos; 
+    Quaternion initialRot;
     
     void Start()
     {
+        initialPos = transform.position;
+        initialRot = transform.rotation;
         nonDominantHand = GameObject.FindGameObjectWithTag("NonDominantHand");
     }
 
@@ -14,9 +18,24 @@ public class ActivateBook : MonoBehaviour
     {
         if (toggleFollow)
         {
-            transform.position = nonDominantHand.transform.position;
+            FollowHand();
+        }
+
+        if (!toggleFollow)
+        {
+            IdleState();
         }
     }
-    
-    
+
+    void FollowHand()
+    {
+        transform.position = nonDominantHand.transform.position;
+        transform.rotation = nonDominantHand.transform.rotation;
+    }
+
+    void IdleState()
+    {
+        transform.position = initialPos;
+        transform.rotation = initialRot;
+    }
 }

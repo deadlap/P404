6e7e004dbcfc7ee083c8cabf58fff32940742a52
deadlap <32266596+deadlap@users.noreply.github.com>
2024-04-29 17:21:53 +0200

    noget værre møj

--- Assets/SFX/SoundHandler.cs
+++ Assets/SFX/SoundHandler.cs
@@ -2,25 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using TMPro;
+using System;
 
-public class SoundHandler : MonoBehaviour
-{
+public class SoundHandler : MonoBehaviour {
+    public static SoundHandler Instance;
     [SerializeField] AudioMixer effectMixer;
-
-    string volumeName = "MasterVolume";
-    void SoundLevel(AudioMixer mixer, float newVolume)
-    {
-        mixer.SetFloat(volumeName, newVolume);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]string masterVolumeName;
+    [SerializeField] string musicVolumeName;
+    [SerializeField] TMP_Text masterText;
+    [SerializeField] TMP_Text musicText;
+    [SerializeField] int audioChange;
+    void Start(){
+        Instance = this;
+        SoundLevel();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void SoundLevel() {
+        effectMixer.SetFloat(masterVolumeName, Mathf.Log10(0.001+(float)Int32.Parse(masterText.text)/100f)*20);
+        effectMixer.SetFloat(musicVolumeName, Mathf.Log10(0.001+(float)Int32.Parse(musicText.text)/100f)*20);
     }
+
+    public void ChangeText(string change){
+        int musicVolume = Int32.Parse(musicText.text);
+        int masterVolume = Int32.Parse(masterText.text);
+        switch(change){
+            case "master+":
+                masterVolume += audioChange;
+                break;
+            case "master-":
+                masterVolume -= audioChange;
+                break;
+            case "music+":
+                musicVolume += audioChange;
+                break;
+            case "music-":
+                musicVolume -= audioChange;
+                break;
+        }
+        masterVolume = Mathf.Clamp(masterVolume, 0, 100);
+        musicVolume = Mathf.Clamp(musicVolume, 0, 100);
+        masterText.text = masterVolume.ToString();
+        musicText.text = musicVolume.ToString();
+        SoundLevel();
+    }
+
 }

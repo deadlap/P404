c4a3030e49aec52edb21d971d31adfdff5e52abb
deadlap <32266596+deadlap@users.noreply.github.com>
2024-05-02 20:59:24 +0200

    liiige fjernet det her funny for nu

--- Assets/Scripts/SetHandednessScale.cs
+++ Assets/Scripts/SetHandednessScale.cs
@@ -6,12 +6,15 @@ public class SetHandednessScale : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         var scaleVector = transform.localScale;
-        if (transform.parent.parent.name == "SpawnHandPosition") {
-            transform.localScale = new Vector3(
-                scaleVector.x*ChooseHand.Instance.handednessScale, scaleVector.y, scaleVector.z);
-        } else {
-            transform.localScale = new Vector3(
-                scaleVector.x, scaleVector.y*ChooseHand.Instance.handednessScale, scaleVector.z);
-        }
+
+        // if (transform.name == Å_hand)
+        // if (transform.parent.name == "SpawnHandPosition" || ) {
+        //     transform.localScale = new Vector3(
+        //         scaleVector.x*ChooseHand.Instance.handednessScale, scaleVector.y, scaleVector.z);
+        // } else {
+        //     transform.localScale = new Vector3(
+        //         scaleVector.x, scaleVector.y*ChooseHand.Instance.handednessScale, scaleVector.z);
+        // }
+        
     }
 }

07467b2f73384aacb52ed7a4f0cfd104d8a2ba43
Akaidus <nikolajjensen6854@gmail.com>
2024-05-02 22:07:45 +0200

    fade part 1

--- Assets/Scripts/LevelSelection.cs
+++ Assets/Scripts/LevelSelection.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using System;
-public class LevelSelection : MonoBehaviour {
 
+public class LevelSelection : MonoBehaviour
+{
     public static event Action<int> ChangeLevelEvent;
     public static void OnChangeLevel(int level) => ChangeLevelEvent?.Invoke(level);
 
@@ -12,53 +13,78 @@ public class LevelSelection : MonoBehaviour {
     // public static void OnNextLevel() => NextLevelEvent?.Invoke();
 
     public static event Action ReloadLevelEvent;
-    public static void  OnReloadLevel () => ReloadLevelEvent?.Invoke();
+    public static void OnReloadLevel() => ReloadLevelEvent?.Invoke();
 
     [SerializeField] List<GameObject> levels;
     [SerializeField] int levelCount; //which level we are on
-
-    void Awake(){
+    int currentLevel;
+    Animator animator;
+    bool isLoading;
+    bool gameLoaded;
+    
+    void Awake()
+    {
         levelCount = 0;
+        animator = GetComponent<Animator>();
     }
 
-    void Start(){
+    void Start()
+    {
         ReloadLevel();
     }
 
-    void OnEnable() {
-        ChangeLevelEvent += ChangeLevel;
+    void OnEnable()
+    {
+        ChangeLevelEvent += BeginLevelChange;
         ReloadLevelEvent += ReloadLevel;
         SceneManager.sceneLoaded += ReloadLevel;
     }
 
-    void OnDisable() {
-        ChangeLevelEvent -= ChangeLevel;
+    void OnDisable()
+    {
+        ChangeLevelEvent -= BeginLevelChange;
         ReloadLevelEvent -= ReloadLevel;
         SceneManager.sceneLoaded -= ReloadLevel;
     }
 
-    void ReloadLevel(Scene scene, LoadSceneMode mode){
-        ChangeLevel(levelCount);
-    }
-    void ReloadLevel(){
-        ChangeLevel(levelCount);
+    void ReloadLevel(Scene scene, LoadSceneMode mode)
+    {
+        BeginLevelChange(levelCount);
     }
 
-    void ChangeLevel(int levelNumber) {
+    void ReloadLevel()
+    {
+        BeginLevelChange(levelCount);
+    }
+
+    void BeginLevelChange(int levelNumber)
+    {
+        if(isLoading) return;
+        isLoading = true;
+        currentLevel = levelNumber;
         if (levelCount < levelNumber || levelCount > levelNumber)
             levelCount = levelNumber;
-        
-        foreach (Transform child in gameObject.transform) {
-	        GameObject.Destroy(child.gameObject);
+        if (gameLoaded || levelCount != 0)
+            animator.SetTrigger("FadeLevel");
+        if (levelCount == 0)
+            ChangeLevel();
+    }
+
+    public void ChangeLevel()       //accessed by animator
+    {
+        foreach (Transform child in gameObject.transform)
+        {
+            GameObject.Destroy(child.gameObject);
         }
-        
+
         var key = GameObject.FindGameObjectWithTag("Key");
         if (key != null)
             GameObject.Destroy(key);
-        
-        var newObject = Instantiate(levels[levelNumber], gameObject.transform);
+
+        var newObject = Instantiate(levels[currentLevel], gameObject.transform);
         newObject.SetActive(true);
         PlayerSpawnPoint.Instance.ResetPlayerPos();
+        gameLoaded = true;
+        isLoading = false;
     }
-
 }
